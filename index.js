1b1b827a0aaa2bad59b1ad88431741f9b456feaf
Jason Vance <jbvance@gmail.com>
2017-12-23 12:36:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function Dog(name, breed, age) {
   this.age = age;
 }
 
-function Cat(name, breed, age){
+function Cat(name, breed, age) {
   this.name = name;
   this.breed = breed;
   this.age = age;
 }
 
-function Ferret (name, age, foodRequirements) {
-  this.name = name
-  this.age = age
-  this.foodRequirements = foodRequirements
+function Ferret(name, age, foodRequirements) {
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = foodRequirements;
 }
