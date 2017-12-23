657f9230c1f38c0a6f41626a79a0bdb6237ec501
Jason Vance <jbvance@gmail.com>
2017-12-23 12:36:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 function Dog(name, breed, age) {
-  {this} = name, breed, age
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Cat(name, breed, age){
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Ferret (name, age, foodRequirements) {
+  this.name = name
+  this.age = age
+  this.foodRequirements = foodRequirements
 }
